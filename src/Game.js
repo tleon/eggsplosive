46997ffeeda50bcbce34e2e31e9cbe403f369482
Jean Carrillo <jeancarrillo@gmail.com>
2019-04-18 14:32:29 +0200

    components import

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import Player from './Player';
+import Collectable from './Collectable';
+import Infos from './Infos';
+import Obstacles from './Obstacles';
 
 class Game extends Component {
 
   render() {
     return (
       <div className="App">
-
-
+        <Player />
+        <Collectable />
+        <Infos />
+        <Obstacles />
       </div>
     );
   }
--- src/Kitchen.js
+++ src/Kitchen.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Recipe from './Recipe';
+import Egg from './Egg';
+import Cauldron from './Cauldron';
+import Inventory from './Inventory';
 
 class Kitchen extends Component {
-
   render() {
     return (
       <div className="App">
-
-
+        <Recipe />
+        <Cauldron />
+        <Egg />
+        <Inventory />
       </div>
     );
   }
