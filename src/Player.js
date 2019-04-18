6b876e637bb4e8d28595ea4eb7f75cf53ae6a552
Justine Vareille <vareille.justine@gmail.com>
2019-04-18 20:04:43 +0200

    blabla

--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import './Player.css';
 
 class Player extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      x: 50,
+      y: 80,
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.move, false);
+  }
+
+  move=event =>{
+    if (event.keyCode === 37) {
+      let { x } = this.state
+      x -= 10
+      this.setState({ x })
+      console.log(this.state.x)
+    }
+    if (event.keyCode === 39) ; 
+  }
 
   render() {
     return (
-      <div className="App">
+      <div className="Player">
 
 
       </div>
