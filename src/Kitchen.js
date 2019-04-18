f8bf9888711960b2a2cf306a0d4f3edee9134057
David <belmontecast@gmail.com>
2019-04-18 16:19:37 +0200

    backgroundKitchen

--- src/Kitchen.js
+++ src/Kitchen.js
@@ -3,11 +3,14 @@ import Recipe from './Recipe';
 import Egg from './Egg';
 import Cauldron from './Cauldron';
 import Inventory from './Inventory';
+import cauldron from './assets/cauldron.png'
+import './Kitchen.css';
 
 class Kitchen extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Background">
+        <img src={cauldron} alt='cauldron' className='cauldron'/>
         <Recipe />
         <Cauldron />
         <Egg />
