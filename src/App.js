f3c15dcbb6183519fa7182ce707579a12668d2cd
Virgile Vercellis <vmv92i@hotmail.fr>
2019-04-18 15:11:11 +0200

    Route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Collection from './Collection';
+import Kitchen from './Kitchen';
+import Game from './Game';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-
-
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/Collection" component={Collection} />
+            <Route path="/Kitchen" component={Kitchen} />
+            <Route path="/Game" component={Game} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
