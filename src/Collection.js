c41df3844bde17d1c05295848c79d27265286923
tleon <leone.thomas.92@gmail.com>
2019-04-18 18:56:06 +0200

    parsing API and displaing the data for the collection

--- src/Collection.js
+++ src/Collection.js
@@ -1,11 +1,78 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { MDBCol, MDBCardBody, MDBCardTitle, MDBCardText, MDBCard, MDBCardImage } from "mdbreact";
+
 
 class Collection extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      eggs: [],
+      characters: [],
+      border: {
+        'junk': '#4B515D',
+        'basic': '#33b5e5',
+        'fine': '#00C851',
+        'rare': '#ffbb33',
+        'legendary': '#FF8800',
+        'exotic': '#9933CC',
+        'masterwork': '#CC0000',
+        'ascended': '#c0ca33'
+      }
+    }
+    this.getEggs();
 
+  }
+  getEggs = () => {
+    axios.get('http://easteregg.wildcodeschool.fr/api/eggs').then(r => {
+      const eggs = r.data;
+      this.setState({ eggs });
+    })
+  }
+  getCharacters = () => {
+    axios.get('http://easteregg.wildcodeschool.fr/api/characters').then(r => {
+      const characters = r.data;
+      this.setState({ characters });
+    })
+  }
+  toggle = tab => () => {
+    if (this.state.activeItem !== tab) {
+      this.setState({
+        activeItem: tab
+      });
+    }
+  }
   render() {
+    const { eggs } = this.state;
+    const { border } = this.state;
     return (
-      <div className="App">
-      
+      <div className="App container-fluid">
+        <div className="row d-flex justify-content-center" style={{ margin: "30px" }}>
+          <h1>Collection</h1>
+        </div>
+
+
+        <div className="row">
+
+          {eggs.map((egg, index) =>
+            <div className="col-3">
+              <MDBCol style={{ marginBottom: "10px" }}>
+                <MDBCard style={{ width: "22rem", border: `5px solid ${border[egg.rarity]}` }}>
+                  <div className="row d-flex justify-content-center">
+                    <MDBCardImage style={{ width: "7rem" }} className="img-fluid" key={`img-${index}`} src={egg.image} waves />
+                  </div>
+                  <MDBCardBody>
+                    <MDBCardTitle key={`name-${index}`}>{egg.name}</MDBCardTitle>
+                    <MDBCardText>
+                      {egg.power}
+                    </MDBCardText>
+                  </MDBCardBody>
+                </MDBCard>
+              </MDBCol>
+            </div>
+          )}
+        </div>
+
       </div>
     );
   }
