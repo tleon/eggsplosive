128d8781fc63c29713f864f82ff80f30ae7c776c
tleon <leone.thomas.92@gmail.com>
2019-04-19 04:09:22 +0200

    go comme ca

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import bootstrap from 'bootstrap/dist/css/bootstrap-grid.css';
+import bootstrap from './bootstrap-grid.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
