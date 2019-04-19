a3bbe667e7b460b8f60ae72b28ac3f41ff0a7471
David <belmontecast@gmail.com>
2019-04-19 03:29:26 +0200

    infos sans panier

--- src/Infos.js
+++ src/Infos.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import milkimg from './assets/milk.png';
+import eggsimg from './assets/egg.png';
+import chocolatesimg from './assets/chocolate.png';
+import character from './assets/RogerRabbit.jpg';
 import './Infos.css';
+
 
 class Infos extends Component {
 
@@ -7,9 +12,10 @@ class Infos extends Component {
     const { milk, eggs, chocolates } = this.props;
     return (
       <div className="Infos">
-        <span>Milk : {milk}</span>
-        <span>Eggs : {eggs}</span>
-        <span>Chocolates : {chocolates}</span>
+        <span><img src={milkimg} alt='milk'/> {milk}</span>
+        <span><img src={eggsimg} alt='milk'/> {eggs}</span>
+        <span><img src={chocolatesimg} alt='milk'/> {chocolates}</span>
+        <span><img src={character} alt='RogerRabbit'/> Roger Rabbit</span>
       </div>
     );
   }
