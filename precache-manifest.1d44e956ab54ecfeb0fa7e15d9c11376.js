947a1c80b7e6f8adb81ccb2c960462547fd912f0
tleon <leone.thomas.92@gmail.com>
2019-05-08 23:59:32 +0200

    Updates

---
+++ precache-manifest.1d44e956ab54ecfeb0fa7e15d9c11376.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "16277a7e117a8f154109ecdae350e45e",
+    "url": "/eggsplosive/static/media/egg.16277a7e.png"
+  },
+  {
+    "revision": "02d53dda747ce7a7f3bb",
+    "url": "/eggsplosive/static/css/main.eddecf3d.chunk.css"
+  },
+  {
+    "revision": "724268b8c7ea2c1a8760",
+    "url": "/eggsplosive/static/js/runtime~main.bc7c03d5.js"
+  },
+  {
+    "revision": "7c7c42cad821d1485b85",
+    "url": "/eggsplosive/static/js/2.df17141f.chunk.js"
+  },
+  {
+    "revision": "e062d4d5693284c75e01513d5238b1fe",
+    "url": "/eggsplosive/static/media/milk.e062d4d5.png"
+  },
+  {
+    "revision": "9ad77424fb315ae4bffd5a95954b0915",
+    "url": "/eggsplosive/static/media/chocolate.9ad77424.png"
+  },
+  {
+    "revision": "02d53dda747ce7a7f3bb",
+    "url": "/eggsplosive/static/js/main.5cb41dc9.chunk.js"
+  },
+  {
+    "revision": "d983818ac8817d87bc2f30f7033ef10a",
+    "url": "/eggsplosive/static/media/EasterFont.d983818a.ttf"
+  },
+  {
+    "revision": "960067bbceab3524ee889553b855e810",
+    "url": "/eggsplosive/static/media/BackgroundKitchen.960067bb.jpg"
+  },
+  {
+    "revision": "ef9ebdce9f47123c870e9908ef34f975",
+    "url": "/eggsplosive/static/media/Lapin.ef9ebdce.png"
+  },
+  {
+    "revision": "67e7e17c310d4825bc589cafa029534c",
+    "url": "/eggsplosive/static/media/BackgroundGame.67e7e17c.png"
+  },
+  {
+    "revision": "f8fb6b86709ed00cd7b68921b2a633e1",
+    "url": "/eggsplosive/index.html"
+  }
+];
